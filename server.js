bf83e96b59eb56453e483713c2218bee0ce743c0
Maryna Pylypchenko <maryna.pylypchenko@gmail.com>
2023-02-11 14:19:26 -0800

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express=require("express")
 const cors=require("cors")
-//const path =require('path')
+const path =require('path')
 const contactForm = require("./route/contactForm")
 const app =express();
  
@@ -11,10 +11,15 @@ app.use(cors())
 
 app.use("/", contactForm)
 
-
+if(process.env.Node_ENV ==="production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req,res)=>(
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    ))
+}
 const port =process.env.PORT || 3000;
 
-app.listen(port, console.log(`server listening to port 5000`))
+app.listen(port, console.log(`server listening to port 3000`))
 
 
 
